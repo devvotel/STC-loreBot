bfa15ba3c342b88dd8bb52a30a86847207437742
DANIIL_PKHAKADZE <s4101606@student.rmit.edu.au>
2025-11-05 00:10:04 +1100

    figuring out an appropriate rate limit

--- plaintext_scraper.py
+++ plaintext_scraper.py
@@ -4,13 +4,14 @@ import json
 
 lexicanum = mediawiki.MediaWiki()
 lexicanum.set_api_url("https://wh40k.lexicanum.com/mediawiki/api.php")
-lexicanum.user_agent = "Saving titles of all the pages for personal project"
+lexicanum.user_agent = "Downloading the plaintext of pages for a personal project"
 lexicanum.rate_limit = True
-lexicanum.rate_limit_min_wait = datetime.timedelta(seconds=1)
+lexicanum.rate_limit_min_wait = datetime.timedelta(milliseconds=500)
 
 with open("page_titles.json","r", encoding="utf-8") as jsonFile:
     pages = json.load(jsonFile)
 
+print("START")
 for i in range (5):
     curr = lexicanum.page(pages[i])
     print(curr.content)
